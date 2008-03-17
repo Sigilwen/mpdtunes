b46450ad3840aea9f3fc892fabd927ca20de05bc
robclark <robclark@483d3f31-fe49-0410-b2df-4364ea8704b8>
2008-03-17 02:47:58 +0000

    now artists menu works

---
+++ atvmpdcontrol/MPDPlayerArtistsController.h
@@ -0,0 +1,20 @@
+//
+//  MPDPlayerArtistsController.h
+//  mpdctrl
+//
+//  Created by Rob Clark on 3/16/08.
+//  Copyright 2008 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import "MPDPlayerController.h"
+
+@interface MPDPlayerArtistsController : MPDPlayerController {
+  NSString *_genre;
+}
+
+- (id) initWithScene: (BRRenderScene *) scene mpdConnection: (MPDConnection *) mpdConnection genre: (NSString *)genre;
+- (id) itemForRow: (long) row;
+- (void) itemSelected: (long) row;
+
+@end
