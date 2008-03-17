2125928247d661c5b5d88ff169d6601ab5aaf938
robclark <robclark@483d3f31-fe49-0410-b2df-4364ea8704b8>
2008-03-17 05:52:17 +0000

    now adding to playlist works

--- trunk/atvmpdcontrol/MPDPlayerController.m
+++ trunk/atvmpdcontrol/MPDPlayerController.m
@@ -8,6 +8,7 @@
 
 #import "MPDPlayerController.h"
 #import "mpdctrlApplianceController.h"
+#import "libmpd/libmpd.h"
 
 
 @implementation MPDPlayerController
@@ -16,7 +17,7 @@
 {
   if ( [super initWithScene: scene] == nil )
     return ( nil );
-
+  
 	_mpdConnection = nil;
 	
 	return ( self );
@@ -103,7 +104,7 @@
 
 - (void) onStatusChanged:(ChangedStatusType)what
 {
-
+  
 }
 
 - (void) onConnectionLost
@@ -114,13 +115,82 @@
 
 
 - (void) addToPlaylist: (MPDConnection *)mpdConnection
-    genre: (NSString *)genre
-    artist: (NSString *)artist
-    album: (NSString *)album
-    song: (NSString *)song
+                 genre: (NSString *)genre
+                artist: (NSString *)artist
+                 album: (NSString *)album
+                  song: (NSString *)song
 {
-  printf("addToPlaylist: genre=%s artist=%s album=%s song=%s\n", genre, artist, album, song);
+  MpdData *data;
+  const char *cGenre;
+  const char *cArtist;
+  const char *cAlbum;
+  const char *cSong;
+  
+  mpd_database_search_start([mpdConnection object], TRUE);
+  if( genre != NULL )
+  {
+    cGenre = [genre UTF8String];
+    mpd_database_search_add_constraint([mpdConnection object], MPD_TAG_ITEM_GENRE, cGenre);
+  }
+  if( artist != NULL )
+  {
+    cArtist = [artist UTF8String];
+    mpd_database_search_add_constraint([mpdConnection object], MPD_TAG_ITEM_ARTIST, cArtist);
+  }
+  if( album != NULL )
+  {
+    cAlbum = [album UTF8String];
+    mpd_database_search_add_constraint([mpdConnection object], MPD_TAG_ITEM_ALBUM, cAlbum);
+  }
+  if( song != NULL )
+  {
+    cSong = [song UTF8String];
+    mpd_database_search_add_constraint([mpdConnection object], MPD_TAG_ITEM_TITLE, cSong);
+  }
+  for( data = mpd_database_search_commit([mpdConnection object]);
+      data != NULL;
+      data = mpd_data_get_next(data) )
+  {
+    if( data->type == MPD_DATA_TYPE_SONG )
+    {
+      printf("mpd_playlist_queue_add(%s)\n", data->song->file);
+      mpd_playlist_queue_add([mpdConnection object], data->song->file);
+    }
+  }
+  // last mpd_data_get_next() free's the search
+  printf("mpd_playlist_queue_commit()\n");
+  mpd_playlist_queue_commit([mpdConnection object]);
 }
 
+/*
+ [_currentSongIds addObject: [NSString stringWithCString:pData->song->file encoding:NSUTF8StringEncoding] ];
+- (void)onSong
+{
+        int i;
+        MADListControl* list = [_listCtrlArray objectAtIndex:2];
+        [list stopScrolling];
+        int selectedSong = [list selection];
+        
+        if(selectedSong == 0)
+        {
+                [_selectedSongIds addObjectsFromArray: [[_dataSrcArray objectAtIndex:2] getCurrentSongIds ] ];
+        }
+        else
+        {
+                NSMutableArray* songIds = [[_dataSrcArray objectAtIndex:2] getCurrentSongIds ];
+                [_selectedSongIds addObject:[songIds objectAtIndex:selectedSong-1 ] ];
+        }
+        
+        for(i=0;i<[_selectedSongIds count];i++)
+        {
+                NSString * sFile = [_selectedSongIds objectAtIndex:i];
+                mpd_playlist_queue_add([_mpdConnection object],(char*)[sFile cStringUsingEncoding:NSUTF8StringEncoding]);
+        }
+        
+        mpd_playlist_queue_commit([_mpdConnection object]);
+                
+        [[self stack]popController];
+}
+*/
 
 @end
